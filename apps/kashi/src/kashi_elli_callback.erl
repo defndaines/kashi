daa0ba46ee970ec92ad71dcd9a3fc38b29effb79
Michael Daines <mdaines@rubiconproject.com>
2017-01-05 14:10:41 -0800

    Handles JSON parsing errors and non-JSON Content-Type

--- apps/kashi/src/kashi_elli_callback.erl
+++ apps/kashi/src/kashi_elli_callback.erl
@@ -8,12 +8,24 @@ handle(Req, Args) ->
 	%% Delegate to three-arg handler function.
 	handle(Req#req.method, elli_request:path(Req), Req, Args).
 
+%% Calls returning content should return HTTP code 200.
+%% Calls returning no content in response to valid requests (e.g., an empty bid response which is one option for indicating no-bid, a win notice that does not return markup) should return HTTP 204.
+%% Invalid calls (e.g., a bid request containing a malformed or corrupt payload) should return HTTP 400 with no content.
 handle('POST', [<<"bid">>], Req, _Args) ->
-  JSON = jsx:decode(Req#req.body, [return_maps]),
-  {ok, [{<<"Content-type">>, <<"application/json">>}], jsx:encode(JSON)};
+  case elli_request:get_header(<<"Content-Type">>, Req, <<"application/json">>) of
+    <<"application/json">> ->
+      try jsx:decode(Req#req.body, [return_maps]) of
+        JSON ->
+          {ok, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(JSON)}
+      catch
+        error:badarg -> {400, [], <<"">>}
+      end;
+    _ ->
+      {400, [], <<"">>}
+  end;
 
 handle(_, _, _Req, _Args) ->
-	{404, [], <<"Not Found">>}.
+	{400, [], <<"">>}.
 
 %% @doc: Handle request events, like request completed, exception
 %% thrown, client timeout, etc. Must return 'ok'.
